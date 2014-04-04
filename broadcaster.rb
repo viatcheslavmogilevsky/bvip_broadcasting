d2f2776ff8abe8356cea570c78a53db7b7145de5
Viatcheslav Mogilevsky <triggerwoods91@gmail.com>
2014-04-04 09:00:20 -0700

    Overengine

--- broadcaster.rb
+++ broadcaster.rb
@@ -5,19 +5,13 @@ require './services/redis_connection.rb'
  
 class Broadcaster < Sinatra::Base
 
-  MESSAGE_FIELDS = {
-    user_changes: 'activity_log',
-    company_changes: 'activity_log',
-    general_changes: 'activity_log',
-    status_changes: 'status'
-  }
-
-  def build_channel_map(user_id,company_id)
+  def build_channel_map(user_id,company_id,device_id)
     {
       user_changes: "bvip:user_changes:#{user_id}",
       company_changes: "bvip:company_changes:#{company_id}",
       general_changes: "bvip:general_changes",
-      status_changes: "bvip:status_changes:#{user_id}"
+      disconnect_channel: "bvip:disconnect:#{user_id}",
+      disconnect_device_channel: "bvip:disconnect_device:#{device_id}"
     }
   end
 
@@ -28,16 +22,19 @@ class Broadcaster < Sinatra::Base
 
   get '/stream', provides: 'text/event-stream' do
     redis_client = Redis.new(settings.redis_config)
-    user_id, company_id = redis_client.hmget("bvip:auth_token:#{params[:auth_token]}", 'user_id', 'company_id')
-    channel_map = build_channel_map(user_id, company_id)
-    inverted_map = channel_map.invert
+    user_id, company_id, device_id = redis_client.hmget("bvip:auth_token:#{params[:auth_token]}", 'user_id', 'company_id', 'device_id')
+    puts "Connected: (id: #{user_id}; company_id: #{company_id}; device_id: #{device_id})"
+    channel_map = build_channel_map(user_id, company_id, device_id)
 
     stream :keep_open do |out|
       redis_client.subscribe(*channel_map.values) do |on|
         on.message do |channel, msg|
-          out << "data: #{msg} \n\n"
-          if (channel == channel_map[:status_changes]) && (msg == '1')
+          if (channel =~ /disconnect/)
+            puts "Disconnected: (id: #{user_id}) from channel #{channel}"
             redis_client.unsubscribe(*channel_map.values)
+          else
+            puts "To (id: #{user_id}): #{msg[0..90]}"
+            out << "data: #{msg} \n\n"
           end
         end
 
